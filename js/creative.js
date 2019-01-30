05d5b5aa585b243a590a4212990aa7de7a81285e
shifumix <dev@shifumix.com>
2019-01-30 19:59:26 +0100

    Version corrigée en terme de mise en page

--- js/creative.js
+++ js/creative.js
@@ -30,8 +30,10 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+        document.getElementById("logoF80").style.display="inline";
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      document.getElementById("logoF80").style.display="none";
     }
   };
   // Collapse now if page is not at top
