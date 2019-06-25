584ede594714e7b1e7c5c98c96d992ed1faf3d37
shifumix <dev@shifumix.com>
2019-06-25 07:36:28 +0200

    Version avec le blog

---
+++ test/tools.js
@@ -0,0 +1,25 @@
+function getParam() {
+    var vars = {};
+    window.location.href.replace(location.hash, '')
+        .replace(/[?&]+([^=&]+)=?([^&]*)?/gi, function (m, key, value) { return vars[key] = value !== undefined ? value : ''; });
+    return vars;
+}
+function getServer() {
+    //return "http://localhost:5000";
+    return "https://server.f80.fr:5500";
+}
+function service(serviceName, params, func) {
+    if (params === void 0) { params = ""; }
+    if (func === void 0) { func = null; }
+    var sep = "/";
+    if (params.indexOf("=") > -1)
+        sep = "?";
+    var url = getServer() + "/" + serviceName + sep + params;
+    console.log("Appel de " + url);
+    fetch(url, { headers: { 'Access-Control-Allow-Origin': '*' }, mode: "cors" })
+        .then(function (res) { return res.json(); })
+        .then(function (r) {
+        if (func != null)
+            func(r);
+    });
+}
